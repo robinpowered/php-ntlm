8171f2e02c4da9f1c721181633cfc4752a922599
Trevor Suarez <rican7@gmail.com>
2015-08-04 18:25:22 -0400

    New interface for generating cryptographically- secure random byte strings

---
+++ src/Robin/Ntlm/Crypt/Random/RandomByteGeneratorInterface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Robin NTLM
+ *
+ * @copyright 2015 Robin Powered, Inc.
+ * @link https://robinpowered.com/
+ */
+
+namespace Robin\Ntlm\Crypt\Random;
+
+/**
+ * An generator used to generate cryptographically secure random bytes.
+ *
+ * Useful for generating random binary strings to be used as a "nonce" or as an
+ * initialization vector.
+ */
+interface RandomByteGeneratorInterface
+{
+
+    /**
+     * Generates cryptographically secure random bytes.
+     *
+     * @param int $size The length, in bytes, of the string to generate.
+     * @return string The randomly generated binary string.
+     */
+    public function generate($size);
+}
