cc4ffce21b84c923af8e98b2328386d16388cf58
Trevor Suarez <rican7@gmail.com>
2015-08-04 18:18:43 -0400

    New password credential value object

---
+++ src/Robin/Ntlm/Credential/Password.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Robin NTLM
+ *
+ * @copyright 2015 Robin Powered, Inc.
+ * @link https://robinpowered.com/
+ */
+
+namespace Robin\Ntlm\Credential;
+
+/**
+ * Represents a plain-text password credential.
+ */
+class Password implements CredentialInterface
+{
+
+    /**
+     * Properties
+     */
+
+    /**
+     * The internal string value.
+     *
+     * @type string
+     */
+    private $value;
+
+
+    /**
+     * Methods
+     */
+
+    /**
+     * Constructor
+     *
+     * @param string $plaintext_password The plain-text password string.
+     */
+    public function __construct($plaintext_password)
+    {
+        $this->value = $plaintext_password;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isPlaintext()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __toString()
+    {
+        return $this->getValue();
+    }
+}
