0a3aaacd79e5f9d21b8c5e311787f635b2e583a6
Trevor Suarez <rican7@gmail.com>
2015-08-04 19:07:52 -0400

    Boom! LM hasher done! ... I think

--- src/Robin/Ntlm/Hasher/LmHasher.php
+++ src/Robin/Ntlm/Hasher/LmHasher.php
@@ -11,13 +11,17 @@ namespace Robin\Ntlm\Hasher;
 use Robin\Ntlm\Credential\Hash;
 use Robin\Ntlm\Credential\HashType;
 use Robin\Ntlm\Credential\Password;
+use Robin\Ntlm\Crypt\CipherMode;
+use Robin\Ntlm\Crypt\Des\DesEncrypterInterface;
+use Robin\Ntlm\Crypt\Random\RandomByteGeneratorInterface;
 use SplFixedArray;
 
 /**
  * Uses the "LM hash" computation strategy to hash a {@link Password} credential
  * into a {@link HashCredentialInterface} of {@link HashType::LM}.
  *
- * @link TODO: Include the LM Hash spec here...
+ * @link http://download.microsoft.com/download/9/5/E/95EF66AF-9026-4BB0-A41D-A4F81802D92C/[MS-NLMP].pdf
+ * @link https://msdn.microsoft.com/en-us/library/cc236699.aspx
  */
 class LmHasher implements HasherInterface
 {
@@ -47,10 +51,65 @@ class LmHasher implements HasherInterface
      */
     const PASSWORD_SLICE_LENGTH = 7;
 
+    /**
+     * The length of the randomly generated binary string used for generating
+     * each piece of the resulting hash.
+     *
+     * @type int
+     */
+    const RANDOM_BINARY_STRING_LENGTH = 8;
+
+    /**
+     * The constant known ASCII text to encrypt with the generated keys.
+     *
+     * @link https://tools.ietf.org/html/rfc2433#appendix-A.3
+     * @link http://download.microsoft.com/download/9/5/E/95EF66AF-9026-4BB0-A41D-A4F81802D92C/[MS-NLMP].pdf
+     * @link https://msdn.microsoft.com/en-us/library/cc236699.aspx
+     * @type string
+     */
+    const ENCRYPT_DATA_CONSTANT = 'KGS!@#$%';
+
+
+    /**
+     * Properties
+     */
+
+    /**
+     * The DES encryption engine used to generate the hash.
+     *
+     * @type DesEncrypterInterface
+     */
+    private $des_encrypter;
+
+    /**
+     * The generator used to generate cryptographically secure random bytes to
+     * provide an initialization vector for encryption.
+     *
+     * @type RandomByteGeneratorInterface
+     */
+    private $random_byte_generator;
+
 
     /**
      * Methods
      */
+
+    /**
+     * Constructor
+     *
+     * @param DesEncrypterInterface $des_encrypter The DES encryption engine
+     *   used to generate the hash.
+     * @param RandomByteGeneratorInterface $random_byte_generator Used to
+     *   generate cryptographically secure random bytes to provide an
+     *   initialization vector for encryption.
+     */
+    public function __construct(
+        DesEncrypterInterface $des_encrypter,
+        RandomByteGeneratorInterface $random_byte_generator
+    ) {
+        $this->des_encrypter = $des_encrypter;
+        $this->random_byte_generator = $random_byte_generator;
+    }
 
     /**
      * {@inheritDoc}
@@ -67,7 +126,21 @@ class LmHasher implements HasherInterface
 
         $halves = str_split($string_password, static::PASSWORD_SLICE_LENGTH);
 
-        //TODO $binary_hash = ;
+        // Encrpyt each half
+        $binary_hash = array_reduce(
+            $halves,
+            function ($result, $half) {
+                $expanded = static::expand56BitKeyTo64BitKey($half);
+
+                $result .= $this->des_encrypter->encrypt(
+                    $expanded,
+                    static::ENCRYPT_DATA_CONSTANT,
+                    CipherMode::ECB,
+                    $this->random_byte_generator->generate(static::RANDOM_BINARY_STRING_LENGTH)
+                );
+            },
+            ''
+        );
 
         return Hash::fromBinaryString($binary_hash, HashType::LM);
     }
@@ -105,13 +178,5 @@ class LmHasher implements HasherInterface
         }
 
         return $key_64bit;
-    }
-
-    public static function desEncrypt($string, $source = MCRYPT_DEV_URANDOM)
-    {
-        $iv_size = mcrypt_get_iv_size(MCRYPT_DES, MCRYPT_MODE_ECB);
-        $iv = mcrypt_create_iv($iv_size, $source);
-
-        // TODO... Damn.. I gotta abstract this garbage...
     }
 }
