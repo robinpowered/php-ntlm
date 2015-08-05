5d6602dcd11c7522fa223d418c1c99a5f78bf1d2
Trevor Suarez <rican7@gmail.com>
2015-08-05 18:50:27 -0400

    New NTv1 hasher implementation! Woot!

---
+++ src/Robin/Ntlm/Hasher/Nt1Hasher.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Robin NTLM
+ *
+ * @copyright 2015 Robin Powered, Inc.
+ * @link https://robinpowered.com/
+ */
+
+namespace Robin\Ntlm\Hasher;
+
+use InvalidArgumentException;
+use Robin\Ntlm\Credential\Hash;
+use Robin\Ntlm\Credential\HashType;
+use Robin\Ntlm\Credential\Password;
+use Robin\Ntlm\Crypt\Hasher\HasherAlgorithm;
+use Robin\Ntlm\Crypt\Hasher\TypedHasherInterface;
+use Robin\Ntlm\Encoding\EncodingConverterInterface;
+
+/**
+ * Uses the "NT hash" computation strategy to hash a {@link Password} credential
+ * into a {@link HashCredentialInterface} of {@link HashType::NT_V1}.
+ *
+ * Known in internal Microsoft documentation as the "NTOWF" function.
+ *
+ * @link http://download.microsoft.com/download/9/5/E/95EF66AF-9026-4BB0-A41D-A4F81802D92C/[MS-NLMP].pdf
+ * @link https://msdn.microsoft.com/en-us/library/cc236699.aspx
+ */
+class Nt1Hasher implements HasherInterface
+{
+
+    /**
+     * Constants
+     */
+
+    /**
+     * The expected {@link HasherAlgorithm} for the cryptographic hasher.
+     *
+     * @type string
+     */
+    const EXPECTED_HASHER_ALGORITHM = HasherAlgorithm::MD4;
+
+    /**
+     * The character encoding that the hash source (input credential) should be
+     * encoded in for consistent hashing results.
+     *
+     * @type string
+     */
+    const HASH_SOURCE_ENCODING = 'UTF-16LE';
+
+
+    /**
+     * Properties
+     */
+
+    /**
+     * The cryptographic hashing engine used to generate the hash.
+     *
+     * @type TypedHasherInterface
+     */
+    private $crypt_hasher;
+
+    /**
+     * Used to convert encodings of the input credential for hashing.
+     *
+     * @type EncodingConverterInterface
+     */
+    private $encoding_converter;
+
+
+    /**
+     * Methods
+     */
+
+    /**
+     * Constructor
+     *
+     * @param TypedHasherInterface $crypt_hasher The cryptographic hashing
+     *   engine used to generate the hash.
+     * @param EncodingConverterInterface $encoding_converter Used to convert
+     *   encodings of the input credential for hashing.
+     */
+    public function __construct(TypedHasherInterface $crypt_hasher, EncodingConverterInterface $encoding_converter)
+    {
+        if (static::EXPECTED_HASHER_ALGORITHM !== $crypt_hasher->getAlgorithm()) {
+            throw new InvalidArgumentException(
+                sprintf('Invalid crytographic hasher algorithm. Expected "%s"', static::EXPECTED_HASHER_ALGORITHM)
+            );
+        }
+
+        $this->crypt_hasher = $crypt_hasher;
+        $this->encoding_converter = $encoding_converter;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hash(Password $password)
+    {
+        $unicode_password_string = $this->encoding_converter->convert(
+            $password->getValue(),
+            static::HASH_SOURCE_ENCODING
+        );
+
+        $binary_hash = $this->crypt_hasher->update($unicode_password_string)->digest();
+
+        return Hash::fromBinaryString($binary_hash, HashType::NT_V1);
+    }
+}
