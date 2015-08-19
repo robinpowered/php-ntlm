2beb0b38c9c6c4d3ebc81fda338286d6fc5eb8d9
Trevor Suarez <rican7@gmail.com>
2015-08-19 13:30:16 -0400

    Getting a good start on the NTLMv1 implementation of the new `AuthenticateMessageEncoderInterface`

---
+++ src/Robin/Ntlm/Message/NtlmV1AuthenticateMessageEncoder.php
@@ -0,0 +1,256 @@
+<?php
+/**
+ * Robin NTLM
+ *
+ * @copyright 2015 Robin Powered, Inc.
+ * @link https://robinpowered.com/
+ */
+
+namespace Robin\Ntlm\Message;
+
+use Robin\Ntlm\Credential\CredentialInterface;
+use Robin\Ntlm\Credential\HashCredentialInterface;
+use Robin\Ntlm\Credential\HashType;
+use Robin\Ntlm\Crypt\CipherMode;
+use Robin\Ntlm\Crypt\Des\DesEncrypterInterface;
+use Robin\Ntlm\Crypt\Random\RandomByteGeneratorInterface;
+use Robin\Ntlm\Encoding\EncodingConverterInterface;
+use Robin\Ntlm\Hasher\HasherInterface;
+use UnexpectedValueException;
+
+/**
+ * {@inheritDoc}
+ *
+ * Uses the NTLMv1 protocol for encoding the "AUTHENTICATE_MESSAGE".
+ */
+class NtlmV1AuthenticateMessageEncoder implements AuthenticateMessageEncoderInterface
+{
+
+    /**
+     * Constants
+     */
+
+    /**
+     * An 8-byte string denoting the protocol in use.
+     *
+     * @type string
+     */
+    const SIGNATURE = "NTLMSSP\0";
+
+    /**
+     * A 32-bit unsigned integer indicating the message type.
+     *
+     * @type int
+     */
+    const MESSAGE_TYPE = 0x00000003;
+
+    /**
+     * The character encoding used for "OEM" encoded values.
+     *
+     * @type string
+     */
+    const OEM_ENCODING = 'ASCII';
+
+    /**
+     * The character encoding used for Unicode encoded values.
+     *
+     * @type string
+     */
+    const UNICODE_ENCODING = 'UTF-16LE';
+
+    /**
+     * The length of the full DESL (DES "Long") key source before splitting into
+     * blocks, in bytes.
+     *
+     * @link https://msdn.microsoft.com/en-us/library/cc236717.aspx
+     * @type int
+     */
+    const DESL_FULL_KEY_LENGTH = 21;
+
+    /**
+     * The length of each DESL (DES "Long") key block, in bytes.
+     *
+     * @link https://msdn.microsoft.com/en-us/library/cc236717.aspx
+     * @type int
+     */
+    const DESL_KEY_BLOCK_SEGMENT_LENGTH = 7;
+
+
+    /**
+     * Properties
+     */
+
+    /**
+     * Used to convert encodings of strings before adding them to the message.
+     *
+     * @type EncodingConverterInterface
+     */
+    private $encoding_converter;
+
+    /**
+     * Used to create the "LM hash" in the message.
+     *
+     * @type HasherInterface
+     */
+    private $lm_hasher;
+
+    /**
+     * Used to create the "NT hash" in the message.
+     *
+     * @type HasherInterface
+     */
+    private $nt_hasher;
+
+    /**
+     * Used to create encryption initialization vectors and to generate random
+     * client challenges for extended security support.
+     *
+     * @type RandomByteGeneratorInterface
+     */
+    private $random_byte_generator;
+
+    /**
+     * Used to compute challenge responses for the different message hashes.
+     *
+     * @type DesEncrypterInterface
+     */
+    private $des_encrypter;
+
+
+    /**
+     * Methods
+     */
+
+    /**
+     * Constructor
+     *
+     * @param EncodingConverterInterface $encoding_converter Used to convert
+     *   encodings of strings before adding them to the message.
+     * @param HasherInterface $lm_hasher Used to create the "LM hash".
+     * @param HasherInterface $nt_hasher Used to create the "NT hash".
+     * @param RandomByteGeneratorInterface $random_byte_generator Used to create
+     *   encryption initialization vectors and to generate random client
+     *   challenges for extended security support.
+     * @param DesEncrypterInterface $des_encrypter Used to compute challenge
+     *   responses for the different message hashes.
+     */
+    public function __construct(
+        EncodingConverterInterface $encoding_converter,
+        HasherInterface $lm_hasher,
+        HasherInterface $nt_hasher,
+        RandomByteGeneratorInterface $random_byte_generator,
+        DesEncrypterInterface $des_encrypter
+    ) {
+        $this->encoding_converter = $encoding_converter;
+        $this->lm_hasher = $lm_hasher;
+        $this->nt_hasher = $nt_hasher;
+        $this->random_byte_generator = $random_byte_generator;
+        $this->des_encrypter = $des_encrypter;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function encode(
+        $username,
+        $nt_domain,
+        $client_hostname,
+        CredentialInterface $credential,
+        ServerChallenge $server_challenge
+    ) {
+        $negotiate_flags = $server_challenge->getNegotiateFlags();
+        $server_challenge_nonce = $server_challenge->getNonce();
+
+        // If expecting unicode
+        if ((NegotiateFlag::NEGOTIATE_UNICODE & $negotiate_flags) === NegotiateFlag::NEGOTIATE_UNICODE) {
+            $expected_encoding = static::UNICODE_ENCODING;
+        } else {
+            $expected_encoding = static::OEM_ENCODING;
+        }
+
+        // TODO: Generate an encrypted random session key
+        $session_key = '';
+
+        // Convert our provided values to proper encoding
+        $username = $this->encoding_converter->convert($username, $expected_encoding);
+        $nt_domain = $this->encoding_converter->convert(strtoupper($nt_domain), $expected_encoding);
+        $client_hostname = $this->encoding_converter->convert(strtoupper($client_hostname), $expected_encoding);
+        $session_key = $this->encoding_converter->convert(strtoupper($session_key), $expected_encoding);
+
+        // Default hash and challenge responsevalues
+        $lm_hash = null;
+        $nt_hash = null;
+        $lm_challenge_response = null;
+        $nt_challenge_response = null;
+
+        if ($credential->isPlaintext()) {
+            $lm_hash = $this->lm_hasher->hash($credential);
+            $nt_hash = $this->nt_hasher->hash($credential);
+        } elseif ($credential instanceof HashCredentialInterface) {
+            switch ($credential->getType()) {
+                case HashType::LM:
+                    $lm_hash = $credential;
+                    break;
+                case HashType::NT_V1:
+                    $nt_hash = $credential;
+                    break;
+                default:
+                    throw new UnexpectedValueException('Unsupported hash credential type');
+            }
+        }
+
+        if (null !== $lm_hash) {
+            $lm_challenge_response = $this->calculateChallengeResponseData($lm_hash, $server_challenge_nonce);
+        }
+
+        if (null !== $nt_hash) {
+            $nt_challenge_response = $this->calculateChallengeResponseData($nt_hash, $server_challenge_nonce);
+        }
+
+        // Prepare a binary string to be returned
+        $binary_string = '';
+
+        $binary_string .= static::SIGNATURE; // 8-byte signature
+        $binary_string .= pack('V', static::MESSAGE_TYPE); // 32-bit unsigned little-endian
+
+        return $binary_string;
+    }
+
+    /**
+     * Calculates a response to a server challenge for a given credential hash.
+     *
+     * @param HashCredentialInterface $hash_credential The authentication
+     *   credential hash to compute the response for.
+     * @param string $nonce The 64-bit (8-byte) unsigned server-sent "nonce"
+     *   (number used once) represented as a binary numeric string.
+     * @return string The calculated challenge response data as a binary string.
+     */
+    public function calculateChallengeResponseData(HashCredentialInterface $hash_credential, $nonce)
+    {
+        // Nul pad the credential hash to the full key size
+        $padded_hash = pack(
+            'a'. static::DESL_FULL_KEY_LENGTH,
+            $hash_credential->getValue()
+        );
+
+        $key_blocks = str_split($padded_hash, static::DESL_KEY_BLOCK_SEGMENT_LENGTH);
+
+        $binary_data = array_reduce(
+            $key_blocks,
+            function ($result, $key_block) use ($nonce) {
+                // Generate an initialization vector equal to the length of the nonce
+                $initialization_vector = $this->random_byte_generator->generate(strlen($nonce));
+
+                return $result . $this->des_encrypter->encrypt(
+                    $key_block,
+                    $nonce,
+                    CipherMode::ECB,
+                    $initialization_vector
+                );
+            },
+            ''
+        );
+
+        return $binary_data;
+    }
+}
